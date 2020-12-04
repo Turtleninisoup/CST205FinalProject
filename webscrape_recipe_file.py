9da3f7c4e8c2cb42631cc0b3cbe01057545cba7a
Cathy Hsu <chsucathy@gmail.com>
2020-12-04 11:23:26 -0800

    Add Charlie's webscrape recipe file

---
+++ webscrape_recipe_file.py
@@ -0,0 +1,98 @@
+website_recipe_info = [
+	{
+		"recipe_url" : "http://www.thelittlepine.com/tabouleh/&amp;disp=441132",
+		"tags" : "3 generations of Tabouleh makers have gone into perfecting and simplifying this Tabouleh Recipe!",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1002015.jpg"
+	},
+	{
+		"recipe_url" : "http://www.passmesometasty.com/amazing-gf-classic-lasagna/&amp;disp=441131",
+		"tags" : "Amazing GF Classic Lasagna that requires no noodle pre-boiling! Layered with a hearty sauce, all the good cheeses, and super tasty gf lasagna noodles.",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1002012.jpg"
+	},
+	{
+		"recipe_url" : "http://feedmephoebe.com/2016/07/speedy-five-spice-pulled-pork-burrito-bowls-with-quinoa-and-kefir-jalapeno-crema/&amp;disp=441130",
+		"tags" : "Quick pulled port burrito bowls with quinoa and kefir-jalapeno crema.",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1001985.jpg"
+	},
+	{
+		"recipe_url" : "http://veganhuggs.com/lentil-mushroom-tacos-w-mango-salsa/&amp;disp=441129",
+		"tags" : "Taco night will never be the same after you try these Lentil Mushroom Tacos with Mango Salsa. They are sweet, savory and down right delicious!",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1001983.jpg"
+	},
+	{
+		"recipe_url" : "http://www.thecookierookie.com/banana-split-dump-cake/&amp;disp=441128",
+		"tags" : "Make dessert easy and delish with Banana Split Dump Cake! Best ever!",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1001978.jpg"
+	},
+	{
+		"recipe_url" : "https://myyellowapron.com/2016/07/06/chocolate-banana-bread-with-nutella-glaze/&amp;disp=441127",
+		"tags" : "This Nutella Glazed Chocolate Banana Bread makes a great decadent dessert. Cocoa powder give it s deep chocolate flavor and bananas make it moist and soft.",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1001973.jpg"
+	},
+	{
+		"recipe_url" : "http://veganheritagepress.com/2016/07/vegan-chocolate-chip-coffee-cake-cherries-bacon/&amp;disp=441126",
+		"tags" : "Chocolate Chip Coffee Cake with Cherries and Bacon",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1001969.jpg"
+	},
+	{
+		"recipe_url" : "http://www.howsweeteats.com/2014/01/honey-chipotle-chicken-bowls/&amp;disp=441125",
+		"tags" : "honey chipotle chicken bowls",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1001959.jpg"
+	},
+	{
+		"recipe_url" : "http://yummyworkshop.com/2016/07/21/kinako-linzer-cookies/&amp;disp=441124",
+		"tags" : "Kinako Linzer Cookies - the nutty flavour of the kinako creates an unusual yet delicious combination with the floral raspberry jam.",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1001952.jpg"
+	},
+
+	{
+		"recipe_url" : "https://winosity.com/creamy-mango-panna-cotta-recipe/&amp;disp=441122",
+		"tags" : "Always finish your meal with a delectable dessert such as this mango panna cotta recipe! A luscious mango dessert that will capture everyone’s attention! An easy summer dessert that will satisfy your taste buds on a hot aftern",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1084044.jpg"
+	},
+	{
+		"recipe_url" : "https://ohsweetbasil.com/instant-pot-sloppy-joes-recipe/&amp;disp=441121",
+		"tags" : "The BEST Instant Pot Sloppy Joes Recipe coming at ya!! Rich and smoky sloppy joe sauce full of tender ground beef, all piled up on buttered buns. Now that’s a kid-friendly dinner!",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1084043.jpg"
+	},
+	{
+		"recipe_url" : "https://www.nithisclickncook.com/crispy-kothambir-vadi/&amp;disp=441120",
+		"tags" : "This Kothambir Vadi is a lip-smacking snack that you can make in no time! It is wonderfully crunchy, healthy, filling and satisfying delicious recipe! This to go-to snack whenever you want something quick, simple recipe with min",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1084034.jpg"
+	},
+	{
+		"recipe_url" : "https://www.flourishingkitchen.com/blog/cauliflower-couscous-orange-fennel-salad&amp;disp=441119",
+		"tags" : "Raw cauliflower couscous with fresh herbs, a juicy and delicious citrus orange and fennel salad with mixed olives for lunch or dinner. Ready in 10 minutes. Raw.Vegan.Gluten-Free",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1084031.jpg"
+	},
+	{
+		"recipe_url" : "https://www.spainonafork.com/spanish-beans-garbanzos-con-espinacas-recipe/&amp;disp=441118",
+		"tags" : "The Best-Ever Spanish Beans | Garbanzos con Espinacas Recipe",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1084030.jpg"
+	},
+	{
+		"recipe_url" : "https://bittersweetblog.com/2020/10/05/kombucha-scoby-recipe/&amp;disp=441117",
+		"tags" : "Excess kombucha scoby is blended into the matcha base along with non-dairy milk for a creamy, pleasantly bitter, subtly sweet start.",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1084029.jpg"
+	},
+	{
+		"recipe_url" : "https://www.eva-bakes.com/chocolate-mint-brownie-ice-cream/&amp;disp=441116",
+		"tags" : "If you have leftover mint brownies hanging around the house (ha!), throw them into an ice cream base for an amazing treat!",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1084028.jpg"
+	},
+	{
+		"recipe_url" : "https://tikkido.com/blog/pumpkin-spice-sugar-cookie-recipe&amp;disp=441115",
+		"tags" : "Pumpkin spice sugar cookies are delicious cut out cookies full of fabulous fall flavor with all the classic pumpkin pie spices.",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1084026.jpg"
+	},
+	{
+		"recipe_url" : "https://strengthandsunshine.com/gluten-free-pumpkin-scones-vegan/&amp;disp=441114",
+		"tags" : "Gluten-Free Pumpkin Scones (Vegan, Allergy-Free).  These soft and tender Gluten-Free Pumpkin Scones are vegan, allergy-free, perfectly spiced with fall flavors, lightly topped with icing, and a maple pumpkin spice drizzle! These",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1084024.jpg"
+	},
+	{
+		"recipe_url" : "https://www.wenthere8this.com/roasted-brussels-sprouts/&amp;disp=441113",
+		"tags" : "Tender, caramelized and tossed in a honey balsamic glaze, these roasted Honey Balsamic Brussels Sprouts are simple and easy – perfect for a quick weeknight dinner or a special holiday dinner!",
+		"image_url" : "http://images.tastespotting.com/thumbnails/1084022.jpg"
+	}
+]
