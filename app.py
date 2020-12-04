c9d5b6fc1e186fa189fb514ecd59b458b4cfea23
Cathy Hsu <chsucathy@gmail.com>
2020-12-04 11:59:41 -0800

    Preprocess webscraping data

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from datetime import datetime
+from webscrape_recipe_file import website_recipe_info
+from pprint import pprint
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'csumb-otter'
 bootstrap = Bootstrap(app)
 
-class Playlist(FlaskForm):
+class RecipeSearchTerm(FlaskForm):
     song_title = StringField(
         'Keyword', 
         validators=[DataRequired()]
     )
 
 playlist = []
+recipes = []
 
 def store_song(my_song):
     playlist.append(dict(
@@ -23,14 +26,28 @@ def store_song(my_song):
         date = datetime.today()
     ))
 
+def preprocess(): 
+    # recipe_dict = website_recipe_info.copy()
+    for recipe in website_recipe_info: 
+        recipes.append({})
+        recipes[-1]['recipe_url'] = recipe['recipe_url']
+        recipes[-1]['tags'] = recipe['tags'].lower().split()
+        recipes[-1]['image_url'] = recipe['image_url']
+
+    print("##################################")
+    for recipe in recipes: 
+        pprint(recipe)
+
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    form = Playlist()
+    print("HELLO")
+    preprocess()
+    form = RecipeSearchTerm()
     if form.validate_on_submit():
         store_song(form.song_title.data)
         return redirect('/view_playlist')
     return render_template('index.html', form=form)
 
-# @app.route('/view_playlist')
-# def vp():
-#     return render_template('vp.html', playlist=playlist)
+@app.route('/result')
+def vp():
+    return render_template('vp.html', playlist=playlist)
