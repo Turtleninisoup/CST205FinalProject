5d57aa42716542d63cb1b8a1509ce0ae96ebf5ca
Cathy Hsu <chsucathy@gmail.com>
2020-12-04 13:54:13 -0800

    Store search term

--- app.py
+++ app.py
@@ -6,48 +6,63 @@ from wtforms.validators import DataRequired
 from datetime import datetime
 from webscrape_recipe_file import website_recipe_info
 from pprint import pprint
+import sys
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'csumb-otter'
 bootstrap = Bootstrap(app)
 
 class RecipeSearchTerm(FlaskForm):
-    song_title = StringField(
-        'Keyword', 
+    search_term = StringField(
+        'Search Term', 
         validators=[DataRequired()]
     )
 
-playlist = []
+search_term = []
 recipes = []
+matched_recipes = []
 
-def store_song(my_song):
-    playlist.append(dict(
-        song = my_song,
-        date = datetime.today()
-    ))
+def store_search_term(token):
+    search_term = token.lower().split()
+    return search_term
+    # print("search term")
+    # sys.stdout.flush()
+    # print(search_term)
+    # sys.stdout.flush()
 
 def preprocess(): 
-    # recipe_dict = website_recipe_info.copy()
     for recipe in website_recipe_info: 
         recipes.append({})
         recipes[-1]['recipe_url'] = recipe['recipe_url']
         recipes[-1]['tags'] = recipe['tags'].lower().split()
         recipes[-1]['image_url'] = recipe['image_url']
+    # for recipe in recipes: 
+    #     print(recipe)
+    #     sys.stdout.flush()
 
-    print("##################################")
-    for recipe in recipes: 
-        pprint(recipe)
+def search_for_recipe_matches(): 
+    print(search_term)
+    # sys.stdout.flush()
+    for word in search_term: 
+        print(word)
+        sys.stdout.flush()
+        # for recipe in recipes: 
+        #     for tag in recipes['tags']: 
+        #         print(tag)
+        #         sys.stdout.flush()
+    print("END OF SEARCH")
+
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
-    print("HELLO")
     preprocess()
     form = RecipeSearchTerm()
     if form.validate_on_submit():
-        store_song(form.song_title.data)
-        return redirect('/view_playlist')
+        store_search_term(form.search_term.data)
+        return redirect('/result')
     return render_template('index.html', form=form)
 
 @app.route('/result')
 def vp():
-    return render_template('vp.html', playlist=playlist)
+    search_for_recipe_matches()
+    return render_template('result.html')
