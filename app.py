317ae6f6dca4b3645717bf6ae6c46a37c2add0af
Cathy Hsu <chsucathy@gmail.com>
2020-12-10 00:21:03 -0800

    Find recipes that match user search term

--- app.py
+++ app.py
@@ -21,37 +21,39 @@ class RecipeSearchTerm(FlaskForm):
 recipes = []
 matched_recipes = []
 
+# split search term into separate words and convert words to lower case
 def store_search_term(token):
     search_term = token.lower().split()
     return search_term
-    # print("search term")
-    # sys.stdout.flush()
-    # print(search_term)
-    # sys.stdout.flush()
 
+# clean webscrape data
 def preprocess(): 
     for recipe in website_recipe_info: 
         recipes.append({})
         recipes[-1]['title'] = recipe['title']
         recipes[-1]['recipe_url'] = recipe['recipe_url']
+        # split tag into separate words and convert words to lower case 
         recipes[-1]['tags'] = recipe['tags'].lower().split()
         recipes[-1]['image_url'] = recipe['image_url']
-    for recipe in recipes: 
-        print(recipe)
-        sys.stdout.flush()
 
 def search_for_recipe_matches(search_term): 
-    print(search_term)
-    # sys.stdout.flush()
-    for word in search_term: 
-        print(word)
-        sys.stdout.flush()
-        # for recipe in recipes: 
-        #     for tag in recipes['tags']: 
-        #         print(tag)
-        #         sys.stdout.flush()
-    print("END OF SEARCH")
+    # empty array so recipes that matched previous search term aren't included
+    matched_recipes = []
+    # used to check if a recipe has already been added into matched_recipes
+    already_matched_recipe_titles = []
 
+    # for every word in the search_term, go through each recipe's tags and check if the word matches any word in the 
+    # tag. If there is a match and the recipe has not already been added, add it to matched_recipes
+    for word in search_term: 
+        for recipe in recipes: 
+            for tag in recipe['tags']: 
+                if (tag == word and recipe['title'] not in already_matched_recipe_titles):
+                    matched_recipes.append({})
+                    already_matched_recipe_titles.append(recipe['title'])
+                    matched_recipes[-1]['title'] = recipe['title']
+                    matched_recipes[-1]['recipe_url'] = recipe['recipe_url']
+                    matched_recipes[-1]['tags'] = recipe['tags']
+                    matched_recipes[-1]['image_url'] = recipe['image_url']
 
 @app.route('/', methods=('GET', 'POST'))
 def index():
@@ -59,8 +61,6 @@ def index():
     form = RecipeSearchTerm()
     if form.validate_on_submit():
         search_term = store_search_term(form.search_term.data)
-        print("SEARCH TERM = ", search_term)
-        sys.stdout.flush()
         search_for_recipe_matches(search_term)
         return redirect('/result')
     return render_template('index.html', form=form)
